94aee62013f9ace27b3a052352ac4a97a87b8a3a
QuestDragon <126067454+QuestDragon@users.noreply.github.com>
2024-02-12 18:19:09 +0900

    v1.2.0構成のアップロード
    プレリリースのv1.2.0の構成をアップロードしました。

---
+++ AdvancedTurnSignals/AdvancedTurnSignals-PreRelease/AdvancedTurnSignals/AdvancedTurnSignals/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
+// 制御されます。アセンブリに関連付けられている情報を変更するには、
+// これらの属性値を変更してください。
+[assembly: AssemblyTitle("AdvancedTurnSignals")]
+[assembly: AssemblyDescription("The ultimate flexible turn signal script with sound effect playback function.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("QuestDragon")]
+[assembly: AssemblyProduct("AdvancedTurnSignals")]
+[assembly: AssemblyCopyright("Copyright ©  2024 QuestDragon All rights reserved.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible を false に設定すると、このアセンブリ内の型は COM コンポーネントから
+// 参照できなくなります。COM からこのアセンブリ内の型にアクセスする必要がある場合は、
+// その型の ComVisible 属性を true に設定してください。
+[assembly: ComVisible(false)]
+
+// このプロジェクトが COM に公開される場合、次の GUID が typelib の ID になります
+[assembly: Guid("019e78a7-75bc-4d3c-ae89-0b97cfc5d3d4")]
+
+// アセンブリのバージョン情報は、以下の 4 つの値で構成されています:
+//
+//      メジャー バージョン
+//      マイナー バージョン
+//      ビルド番号
+//      リビジョン
+//
+// すべての値を指定するか、次を使用してビルド番号とリビジョン番号を既定に設定できます
+// 既定値にすることができます:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.2.0.0")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
